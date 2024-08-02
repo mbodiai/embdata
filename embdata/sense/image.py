327ae4934bd7bafd9cab37754d88f7af9a1b36f1
chitianhao <chitianhaoxp@gmail.com>
2024-08-02 16:35:05 +0000

    Minor Changes

--- embdata/sense/image.py
+++ embdata/sense/image.py
@@ -229,13 +229,14 @@ class Image(Sample):
 
     @model_validator(mode="before")
     @classmethod
-    def ensure_pil(cls, values: Dict[str, SupportsImage]) -> None:
+    def ensure_pil(cls, values: Dict[str, SupportsImage]) -> None: # noqa
         """Ensure the image is represented as a PIL Image object."""
         sources = ["array", "base64", "path", "url", "bytes"]
         if values.get("pil") is None:
             arg = reduce(lambda x, y: x if x is not None else y, [values.get(key) for key in sources])
             values.update(cls.dispatch_arg(arg, **values))
-        return {key: value for key, value in values.items() if key is not None}
+        values = {key: value for key, value in values.items() if key is not None}
+        return values
 
     @staticmethod
     def pil_to_data(image: PILImage, encoding: str, size=None, mode=None) -> dict:
